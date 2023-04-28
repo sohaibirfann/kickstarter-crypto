530e1aeac7233a5475b8e5ff1abf02bb054b572b
Sohaib Irfan <tihor.personal@gmail.com>
2023-04-28 21:33:59 +0530

    CreateCampaign logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,12 +10,15 @@ const App = () => {
       <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
       </div>
-      
+
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
